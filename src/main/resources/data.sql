3e3a8b8024d99c012b2553d3e9f7b8f835ab8843
mbornard <matisse.bornard@gmail.com>
2023-03-09 15:58:20 +0100

    les classes sont visible sur l'api mais il faut rajouter la table pour Demandedetrajet

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,7 @@ INSERT INTO Utilisateur(userID, nom, prenom, email, notes, dateNaiss) VALUES
 (default, 'Bornard', 'Matisse', 'mb@etud.univ-jfc.fr', 5, '2002-12-25'), --code 1
 (default, 'Miralles', 'Lucas', 'lm@etud.univ-jfc.fr', 5, '2002-11-09'),--code 2
 (default, 'Brignatz', 'Loris', 'lb@etud.univ-jfc.fr', 5, '2002-11-20'), --code 3
-(default, 'Ntamack', 'Romain', 'ntk@etud.univ-jfc.fr', 5, '1999-05-01'); --code 1
+(default, 'Ntamack', 'Romain', 'ntk@etud.univ-jfc.fr', 5, '1999-05-01'); --code 4
 
 INSERT INTO Point(idPoint, nom, adresse, longitude, latitude) VALUES
 
